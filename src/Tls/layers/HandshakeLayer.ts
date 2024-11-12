84b7baa0b246fb11a1e6b834bc455cdb5976cf06
zNadir <115375151+znadir@users.noreply.github.com>
2024-11-12 17:44:32 -0500

    add HandshakeLayer

--- src/Tls/layers/HandshakeLayer.ts
+++ src/Tls/layers/HandshakeLayer.ts
@@ -1,3 +1,24 @@
 import RecordLayer from "./RecordLayer";
+import HandshakeType from "../enum/HandshakeType";
+import ContentType from "../enum/ContentType";
 
-class HandshakeLayer extends RecordLayer {}
+class HandshakeMessage {} // idk yet
+
+class HandshakeLayer {
+	private messageType;
+	private length;
+	private message;
+
+	constructor(messageType: HandshakeType, length: number, message: HandshakeMessage) {
+		this.messageType = messageType;
+		this.length = length;
+
+		switch (messageType) {
+			case HandshakeType.CLIENT_HELLO: {
+			}
+			default: {
+				throw Error("Type de Handshake non supporté");
+			}
+		}
+	}
+}
--- src/Tls/layers/RecordLayer.ts
+++ src/Tls/layers/RecordLayer.ts
@@ -3,7 +3,7 @@ import Layer from "./Layer";
 
 class RecordLayer extends Layer {
 	private contentType; // high-level protocol used to process the fragment
-	private legacyRecordVersion; // 0x0303 for TLS 1.3 other than ClientHello
+	private legacyRecordVersion; // 0x0303 for TLS 1.3 other than ClientHello where may be 0x0301
 	private length; // length in bytes of TLSPlaintext.fragment. Max 2^14 bytes
 	private fragment; // data being transmitted
 
